f041b22accaca4265f31beb46e47053494abbda5
«Nikita <mrnikita.harkov@gmail.com>
2021-02-20 15:22:12 +0200

    Setup navbar, about and error pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo et expedita
+        quasi asperiores veniam molestiae sequi dignissimos, hic iste autem id
+        saepe impedit praesentium quibusdam repudiandae, enim itaque nostrum
+        est?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
